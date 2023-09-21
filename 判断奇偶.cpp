6dbee8a7bcdfa1311fa86baf6146c841b4b838a4
QSDEL <137593187+QSDEL@users.noreply.github.com>
2023-09-21 20:04:04 +0800

    Add files via upload

---
+++ 判断奇偶.cpp
@@ -0,0 +1,26 @@
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+//	int i = 0;
+//	cout << "该程序能判断您输入的数的奇偶\n请键入您要判断的数：" << endl;
+//	cin >> i;
+//
+//	cout << "您输入的数是：" << i << endl;
+//
+//	switch (i % 2)
+//	{
+//	case 1:
+//	{
+//		cout << "该数是奇数" << endl;
+//		break;
+//	}
+//
+//	case 0:
+//	{
+//		cout << "该数是偶数" << endl;
+//	}
+//
+//	return 0;
+//	}
+//}
